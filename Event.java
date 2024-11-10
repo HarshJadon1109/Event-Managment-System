e13f97e51c108da356c7f911f62d970289c32d00
HarshJadon1109 <jadonharsh3011@gmail.com>
2024-11-10 17:42:13 +0530

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,1 +1,20 @@
+import java.util.Date;
+
+public class Event {
+    private int eventId;
+    private String eventName;
+    private Date eventDate;
+    private String eventLocation;
+    private String eventDescription;
+
+    public Event(int eventId, String eventName, Date eventDate, String eventLocation, String eventDescription) {
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventLocation = eventLocation;
+        this.eventDescription = eventDescription;
+    }
+
+    // Getters and Setters
+}
 
